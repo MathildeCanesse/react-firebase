7a175ebd9e8a417bf7afb44a3fc2af4af5f63836
Mathilde Canesse <mathilde.canesse@gmail.com>
2020-01-29 09:18:32 +0100

    [CHORE]: update dependencies (#3)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Router, Route } from "react-router";
-import createHistory from "history/createHashHistory";
+import { Route } from "react-router";
+import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import createSagaMiddleware from "redux-saga";
 import { createStore, applyMiddleware, compose } from "redux";
@@ -16,15 +16,15 @@ import App from "./components/App.jsx";
 
 import "./assets/css/index.css";
 
-const history = createHistory();
-
 firebase.initializeApp(config);
 
 const sagaMiddleware = createSagaMiddleware();
 
 const finalCompose = compose(
   applyMiddleware(thunk, sagaMiddleware),
-  window.devToolsExtension ? window.devToolsExtension() : f => f
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+    : f => f
 );
 
 const store = createStore(allReducers, finalCompose);
@@ -32,10 +32,10 @@ const store = createStore(allReducers, finalCompose);
 sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
-  <Router history={history}>
-    <Provider store={store}>
+  <Provider store={store}>
+    <Router>
       <Route exact path="/" component={App} />
-    </Provider>
-  </Router>,
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
